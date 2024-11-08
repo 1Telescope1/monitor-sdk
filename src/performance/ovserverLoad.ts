fbc763af549436d63a63ef64c9133d8dcae16968
sishi <9987574+shen-qun-deng@user.noreply.gitee.com>
2024-11-08 12:55:08 +0800

    feat: 获取页面load时间

---
+++ src/performance/ovserverLoad.ts
@@ -0,0 +1,28 @@
+export default function observePageLoadTime() {
+  // 记录页面加载开始的时间
+  const startTimestamp = performance.now();
+
+  // 监听 load 事件
+  window.addEventListener('load', () => {
+    // 记录 load 事件触发的时间
+    const loadTimestamp = performance.now();
+
+    // 计算从页面开始加载到 load 事件触发的时间差
+    const loadTime = loadTimestamp - startTimestamp;
+
+    // 构建性能数据对象
+    const reportData = {
+      type: 'performance',
+      subType: 'load',
+      pageUrl: window.location.href,
+      startTime: startTimestamp,
+      loadTime: loadTime
+    };
+
+    // 发送数据
+    console.log(reportData);
+
+    // 如果需要将数据发送到服务器
+    // sendToServer(reportData);
+  });
+}
